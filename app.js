4ecb0c5fb15587b47e5df940c19d9aa93acf7a09
Shawn Adrian <shawn@nerdburn.com>
2014-12-19 15:42:02 -0800

    updating css

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+// import postachio
 var postachio = require('postachio-theme-sdk');
+
+// set options
+var options = {
+  root: __dirname
+}
+
+// start up the server
+var server = postachio(options);
